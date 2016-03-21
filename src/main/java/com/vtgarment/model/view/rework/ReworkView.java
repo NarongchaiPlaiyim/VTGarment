90ce7f3aff5099731050545bab632a9916a6ac29
Narongchai <N.Plaiyim@gmail.com>
2016-03-21 22:09:38 +0700

    Added : recall

--- src/main/java/com/vtgarment/model/view/rework/ReworkView.java
+++ src/main/java/com/vtgarment/model/view/rework/ReworkView.java
@@ -7,37 +7,18 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
-
 @Getter
 @Setter
 @ToString
 public class ReworkView extends View {
+
     private String previousDate;
     private String currentDate;
-    private List<ReworkTableView> reworkTableViewList;
 
     public ReworkView() {
         previousDate = DateUtil.convertDateToString_DD_MM_YYYY(Utils.previousDate());
         currentDate = DateUtil.convertDateToString_DD_MM_YYYY(Utils.currentDate());
-        reworkTableViewList = new ArrayList<>();
-        for (int i = 1; i < 23; i++) {
-            ReworkTableView reworkTableView = new ReworkTableView();
-            reworkTableView.setSutureLine("VSEW00"+String.valueOf(i));
-            int percentOfYesterday = randInt(0, 100);
-            reworkTableView.setPercentOfYesterday(percentOfYesterday+"%");
-            int percentOfToday = randInt(0, 100);
-            reworkTableView.setPercentOfToday(percentOfToday+"%");
-            reworkTableView.setTrends((percentOfYesterday - percentOfToday) +"%");
-            reworkTableViewList.add(reworkTableView);
-        }
     }
 
-    public int randInt(int min, int max) {
-        Random random = new Random();
-        int randomNumber = random.nextInt(max - min) + min;
-        return randomNumber;
-    }
+
 }
