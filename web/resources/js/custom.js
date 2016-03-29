1273ff3216ac35489935f50787727e66015c0b24
pakorn <pakorn.d17@gmail.com>
2016-03-29 11:25:12 +0700

    update

---
+++ web/resources/js/custom.js
@@ -0,0 +1,13 @@
+function Redirect(path) {
+
+    if(path == 'OTP'){
+        window.location = "/test/site/otp.xhtml"
+    } else if(path == 'REWORK'){
+        window.location = "/test/site/rework.xhtml"
+    } else if(path == 'BREAK DOWN'){
+        window.location = "/test/site/breakDown.xhtml"
+    } else if(path == 'OUTSTANDING'){
+        window.location = "/test/site/outstading.xhtml"
+    }
+
+}
